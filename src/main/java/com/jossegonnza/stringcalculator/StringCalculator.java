01a566d69fa40331ed43b1185c42d9cfdca9b294
JosseGonnza <accion.quevedo@gmail.com>
2025-12-04 18:44:10 +0000

    💫 feat: support custom single character separator syntax

--- src/main/java/com/jossegonnza/stringcalculator/StringCalculator.java
+++ src/main/java/com/jossegonnza/stringcalculator/StringCalculator.java
@@ -5,6 +5,20 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
+
+        if (numbers.startsWith("//")) {
+            int newLine = numbers.indexOf("\n");
+            String separator = numbers.substring(2, newLine);
+            String rest = numbers.substring(newLine + 1);
+
+            String[] parts = rest.split(separator);
+            int sum = 0;
+            for (String part : parts) {
+                sum += Integer.parseInt(part);
+            }
+            return sum;
+        }
+
         String nomalized = numbers.replace("\n", ",");
         if (nomalized.contains(",")) {
             String[] parts = nomalized.split(",");
